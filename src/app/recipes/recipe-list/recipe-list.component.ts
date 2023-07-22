fe41ba5121f35585e8b0b2a68eb9b3581b7395e2
mohitSanwal <mohit123ongraph@gmail.com>
2023-07-22 13:24:03 +0530

    recipe list implementation

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,10 +1,20 @@
 import { Component } from '@angular/core';
-
+import {Recipe} from '../recipe.model';
 @Component({
   selector: 'app-recipe-list',
   templateUrl: './recipe-list.component.html',
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent {
+  recipe: Recipe[] = [
+    new Recipe('test recipe', 'this is simply a test recipe', 'https://picturetherecipe.com/wp-content/uploads/2020/04/Vanilla-Cardamom-Kulfi-PTR-Featured-395x500.jpg')
+  ];
 
+  constructor() {
+    
+  }
+
+  ngOnInit() {
+
+  }
 }
