8596a19198ba4d16f8709a278eb868847f27162a
mohitSanwal <mohit123ongraph@gmail.com>
2023-07-22 14:26:11 +0530

    shopping list component view implementation

--- src/app/shopping-list/shopping-list.component.ts
+++ src/app/shopping-list/shopping-list.component.ts
@@ -1,5 +1,5 @@
 import {Component}from '@angular/core';
-
+import {Ingredients} from './ingredients.model'
 @Component({
     selector: 'app-shopping-list',
     templateUrl: './shopping-list.component.html',
@@ -7,5 +7,12 @@ import {Component}from '@angular/core';
 })
 
 export class ShoppingListComponent {
+    ingredients: Ingredients[] = [
+        new Ingredients('apple', 20),
+        new Ingredients('tomatoes', 20)
+    ];
 
+   constructor() {
+
+   }
 }
