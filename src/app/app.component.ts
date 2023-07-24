65075cf16d8900e4d14a06c0a0c91b5c554ca674
mohitSanwal <mohit123ongraph@gmail.com>
2023-07-24 14:44:55 +0530

    event emitteed from header to app component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'shopping-app-angular';
+  featureValue = 'recipe'
+
+  onNavigate(fetureData: string) {
+    console.log('displayInfo', fetureData)
+    this.featureValue = fetureData;
+  }
+
+  ngOnInit() {
+
+  }
+  
 }
--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,12 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.css']
 })
-export class HeaderComponent {
+export class HeaderComponent implements OnInit {
+  isRecipe: boolean = false;
+  isShoppingList: boolean = false;
+  @Output() featureSelected = new EventEmitter<string>();
+  
   constructor() {
     
   }
+  ngOnInit(): void {
+    
+  }
+  
+  onSelect(feature: string) {
+     this.isRecipe = true;
+     this.isShoppingList =false;
+     this.featureSelected.emit(feature)
+     console.log('onRecipesClick', this.featureSelected)
+  }
+
 }
