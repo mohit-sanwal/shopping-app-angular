7f8bc01c4959c29e0ffacd58ed6f5a6d68674ef0
mohitSanwal <mohit123ongraph@gmail.com>
2023-07-24 15:03:53 +0530

    palssing props to child component

--- src/app/recipes/recipe-list/recipe-item/recipe-item.component.ts
+++ src/app/recipes/recipe-list/recipe-item/recipe-item.component.ts
@@ -1,10 +1,13 @@
-import { Component } from '@angular/core';
-
+import { Component, Input } from '@angular/core';
+import {Recipe} from '../../recipe.model';
 @Component({
   selector: 'app-recipe-item',
   templateUrl: './recipe-item.component.html',
   styleUrls: ['./recipe-item.component.css']
 })
 export class RecipeItemComponent {
-
+  @Input() recpEle: Recipe
+  constructor() {
+    this.recpEle = {name: '', imagePath: '', description: ''}
+  }
 }
