6fabd30b346dc98249e1b18cc9ccb1ec6c44cef1
mohitSanwal <mohit123ongraph@gmail.com>
2023-07-28 08:28:13 +0530

    dropdown directive implementation for header and other sectins

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,7 +7,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AppComponent implements OnInit{
   title = 'shopping-app-angular';
-  featureValue = 'shopping'
+  featureValue = 'recipe'
+
+  numbers = [2, 3 ,4,5,6,7,8];
+
+  constructor() {
+    
+  }
 
   onNavigate(fetureData: string) {
     console.log('displayInfo', fetureData)
